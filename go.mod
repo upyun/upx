cd9430dba7513b6a5434d5c88fdc68ea32c591c7
arrebole <concurrent.exec@gmail.com>
2023-05-05 11:26:55 +0800

    refact: 重构进度条

--- go.mod
+++ go.mod
@@ -3,25 +3,27 @@ module github.com/upyun/upx
 go 1.17
 
 require (
+	github.com/arrebole/progressbar v0.0.3
 	github.com/fatih/color v1.15.0
-	github.com/gosuri/uiprogress v0.0.1
 	github.com/stretchr/testify v1.8.1
 	github.com/syndtr/goleveldb v1.0.0
 	github.com/upyun/go-sdk/v3 v3.0.4
 	github.com/urfave/cli v1.22.12
-	golang.org/x/term v0.7.0
+	golang.org/x/term v0.8.0
 )
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
-	github.com/gosuri/uilive v0.0.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- progress.go
+++ progress.go
@@ -1,23 +1,44 @@
 package upx
 
 import (
+	"fmt"
+	"os"
 	"time"
 
-	"github.com/gosuri/uiprogress"
+	"github.com/arrebole/progressbar"
 )
 
-var Progress *uiprogress.Progress
+var enableBar bool = false
 
-func AddBar(id, total int) (*uiprogress.Bar, int) {
-	if id >= len(Progress.Bars) || id < 0 {
-		return Progress.AddBar(total), len(Progress.Bars) - 1
-	} else {
-		Progress.Bars[id] = uiprogress.NewBar(total)
-		return Progress.Bars[id], id
-	}
+func EnableProgressbar() {
+	enableBar = true
 }
 
-func InitProgress() {
-	Progress = uiprogress.New()
-	Progress.RefreshInterval = time.Millisecond * 100
+func AddBar(name string, total int) *progressbar.ProgressBar {
+	if !enableBar {
+		return nil
+	}
+	return progressbar.NewOptions64(
+		int64(total),
+		progressbar.OptionSetDescription(leftAlign(shortPath(name, 30), 30)),
+		progressbar.OptionSetWriter(os.Stderr),
+		progressbar.OptionShowBytes(true),
+		progressbar.OptionSetWidth(10),
+		progressbar.OptionThrottle(65*time.Millisecond),
+		progressbar.OptionShowCount(),
+		progressbar.OptionSpinnerType(14),
+		progressbar.OptionFullWidth(),
+		progressbar.OptionSetRenderBlankState(true),
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "[green]=[reset]",
+			SaucerHead:    "[green]>[reset]",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}),
+		progressbar.OptionOnCompletion(func() {
+			fmt.Fprint(os.Stderr, "\n")
+		}),
+	)
 }
